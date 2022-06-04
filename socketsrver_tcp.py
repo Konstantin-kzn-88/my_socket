6ff8b8f082bfb8ca94a12e7e13582a8820cc43c8
konstantin <kuznetsovkm@yandex.ru>
2022-06-04 09:31:24 +0300

    init

---
+++ socketsrver_tcp.py
@@ -0,0 +1,14 @@
+import socketserver
+
+class EchoTCPHandler(socketserver.BaseRequestHandler):
+
+    def handle(self):
+        data = self.request.recv(1024).strip()
+        print(f'Adress: {self.client_address[0]}')
+        print(f'Data: {data.decode()}')
+        b = bytes('[1,2,3,4]', encoding='utf-8')
+        self.request.sendall(b)
+
+if __name__ == '__main__':
+    with socketserver.TCPServer(('', 8888), EchoTCPHandler) as server:
+        server.serve_forever()
